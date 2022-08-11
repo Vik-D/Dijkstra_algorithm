f6d3801b2c14f788d5bc05bc71e4b2ca3387d0f4
Viktor Didyk <viktordidyk.78@gmail.com>
2022-08-11 16:29:02 +0300

    Update DijkstraProcessing.java

--- DijkstraProcessing.java
+++ DijkstraProcessing.java
@@ -2,63 +2,67 @@ package main;
 
 import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.TreeSet;
 
 public class DijkstraProcessing {
 
 	public static void main(String[] args) {
-		Graph graph = new Graph();
 
-		graph.addNodeAndEdges("A", Arrays.asList(new Edge("B", 5), new Edge("C", 2)));
-		graph.addNodeAndEdges("B", Arrays.asList(new Edge("E", 4), new Edge("D", 2)));
-		graph.addNodeAndEdges("C", Arrays.asList(new Edge("B", 8), new Edge("D", 7)));
-		graph.addNodeAndEdges("D", Arrays.asList(new Edge("F", 1)));
-		graph.addNodeAndEdges("E", Arrays.asList(new Edge("F", 3), new Edge("D", 6)));
-		graph.addNodeAndEdges("F", Arrays.asList(new Edge("F", 0)));
+		Node a = new Node("A");
+		Node b = new Node("B");
+		Node c = new Node("C");
+		Node d = new Node("D");
+		Node e = new Node("E");
+		Node f = new Node("F");
+		a.setEdges(new TreeSet<>(Arrays.asList(new Edge(b, 5), new Edge(c, 2))));
+		b.setEdges(new TreeSet<>(Arrays.asList(new Edge(e, 4), new Edge(d, 2))));
+		c.setEdges(new TreeSet<>(Arrays.asList(new Edge(b, 8), new Edge(d, 7))));
+		d.setEdges(new TreeSet<>(Arrays.asList(new Edge(f, 1))));
+		e.setEdges(new TreeSet<>(Arrays.asList(new Edge(f, 3), new Edge(d, 6))));
+		f.setEdges(new TreeSet<>(Arrays.asList(new Edge(f, 0))));
 
-		String rootNodeName = "A";
 		PriorityQueue<Node> nodesQueue = new PriorityQueue<>();
-		Node rootNode = graph.getNode(rootNodeName);
+		Node rootNode = a;
 		rootNode.setDistance(0);
-		rootNode.setParentName(null);
 		nodesQueue.add(rootNode);
 
 		while (!nodesQueue.isEmpty()) {
 			Node currentNode = nodesQueue.poll();
 			if (currentNode.isProcessed()
-					|| currentNode.getName().equals(currentNode.getEdges().first().getChildName())) {
+					|| currentNode.getName().equals(currentNode.getEdges().first().getChild().getName())) {
 				continue;
 			}
 			for (Edge currentEdge : currentNode.getEdges()) {
-				Node destinationNode = graph.getNode(currentEdge.getChildName());
+				Node destinationNode = currentEdge.getChild();
 				int currentNodeDistance = currentNode.getDistance();
 				int currentEdgeDistance = currentEdge.getDistance();
 				int destinationNodeDistance = destinationNode.getDistance();
 				if (currentNodeDistance + currentEdgeDistance < destinationNodeDistance) {
 					destinationNodeDistance = currentNodeDistance + currentEdgeDistance;
 					destinationNode.setDistance(destinationNodeDistance);
-					destinationNode.setParentName(currentNode.getName());
+					destinationNode.setParent(currentNode);
 				}
 				nodesQueue.add(destinationNode);
 			}
 			currentNode.setProcessed(true);
 		}
 		System.out.println("*******************************");
-		String destinationNodeName = "F";
-		String path = destinationNodeName;
-		Node lastNode = graph.getNode(destinationNodeName);
+		Node lastNode = f;
+		String destinationNodeName = lastNode.getName();
 		int distance = lastNode.getDistance();
+		String path = destinationNodeName;
+
 		while (true) {
-			String previousNodeName = lastNode.getParentName();
-			if (previousNodeName == null) {
+			if (lastNode.getParent() == null) {
 				break;
 			}
+			String previousNodeName = lastNode.getParent().getName();
 			path = previousNodeName + path;
-			lastNode = graph.getNode(previousNodeName);
-
+			lastNode = lastNode.getParent();
 		}
 		String separated = String.join("->", path.split(""));
-		System.out.println(
-				"The shortest distance from node " + rootNodeName + " to " + destinationNodeName + " is " + distance);
+		System.out.println("The shortest distance from node " + rootNode.getName() + " to " + destinationNodeName
+				+ " is " + distance);
 		System.out.println("Path is : " + separated);
 	}
 
